045f366b4f2d4e5f66bcc7819127518784033743
abid <fmaabid@gmail.com>
2018-10-09 19:01:00 +0600

    rendering fetched data

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,30 +7,36 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    posts: []
+  };
+
   componentDidMount() {
     axios.get('https://jsonplaceholder.typicode.com/posts')
       .then(response => {
-        console.log(response)
+        this.setState({posts: response.data});
       });
   }
 
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  render () {
+    const posts = this.state.posts.map(post => {
+      return <Post key={post.id} title={post.title}/>
+    });
+
+    return (
+      <div>
+        <section className="Posts">
+          {posts}
+        </section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
