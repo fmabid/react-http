37848589c039d501e9fd9ba34b19db808d4a8d96
abid <fmaabid@gmail.com>
2018-10-10 00:55:22 +0600

    fetching data on update

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -8,7 +8,8 @@ import './Blog.css';
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPosdId: null
   };
 
   componentDidMount() {
@@ -26,9 +27,17 @@ class Blog extends Component {
       });
   }
 
+  postSelectedHandler = (id) => {
+    this.setState({selectedPosdId: id});
+  };
+
   render () {
     const posts = this.state.posts.map(post => {
-      return <Post key={post.id} title={post.title} author={post.author}/>
+      return <Post
+        key={post.id}
+        title={post.title}
+        clicked={() => this.postSelectedHandler(post.id)}
+        author={post.author}/>
     });
 
     return (
@@ -37,7 +46,7 @@ class Blog extends Component {
           {posts}
         </section>
         <section>
-          <FullPost />
+          <FullPost id={this.state.selectedPosdId}/>
         </section>
         <section>
           <NewPost />
